120892e142f7162790127c11911bd9ed911b8aed
SyedAhris <ahrissyed@gmail.com>
2022-10-30 14:02:50 +0500

    passengers route added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const passengersRoute = require("./routes/passengers");
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
+app.use("/api/passengers", passengersRoute);
 
 app.listen(process.env.PORT_NO, ()=>{
     console.log("backend asdasd server is running at: ", process.env.PORT_NO);
