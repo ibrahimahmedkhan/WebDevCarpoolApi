c93fb298b0d625fc018657ab51b5c214050ceb31
ibrahimahmedkhan <ibrahimahmed.ayesha@gmail.com>
2022-11-28 20:57:22 +0500

    added PORT for heroku

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const express = require("express");
-var cors = require('cors');
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user");
-const passengerRoute = require("./routes/passenger");
-const adminRoute = require("./routes/adminUser");
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const passengerRoute = require('./routes/passenger');
+const adminRoute = require('./routes/adminUser');
 
 const app = express();
 
 dotenv.config(); // necessary to use .env variables
 
-
-//mongo db connection connection
+// mongo db connection connection
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Database Connection Successfull!"))
-.catch((err) =>{
+  .then(() => console.log('Database Connection Successfull!'))
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-//check api
-app.get("/", (req, res)=>{
-    console.log("test is successfull");
-    res.send("Connected");
+// check api
+app.get('/', (req, res) => {
+  console.log('test is successfull');
+  res.send('Connected');
 });
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoute);
-app.use("/api/user", userRoute);
-app.use("/api/passenger", passengerRoute);
-app.use("/api/admin", adminRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
+app.use('/api/passenger', passengerRoute);
+app.use('/api/admin', adminRoute);
 
-app.listen(process.env.PORT_NO, ()=>{
-    console.log("backend asdasd server is running at: ", process.env.PORT_NO);
+app.listen(process.env.PORT || process.env.PORT_NO, () => {
+  console.log('backend asdasd server is running at: ', process.env.PORT_NO);
 });
